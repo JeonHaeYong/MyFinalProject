ddf718d0de147266f435e5423373749e1f960edd
jaeyong <leejaeyong2@naver.com>
2019-07-17 16:22:04 +0900

    update 07_17 16:22

--- src/main/java/kh/spring/fin/HomeController.java
+++ src/main/java/kh/spring/fin/HomeController.java
@@ -1,11 +1,23 @@
 package kh.spring.fin;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.xml.sax.SAXException;
+
+import kh.spring.dto.ApiAbandonedAnimalDTO;
+import kh.spring.serviceImpl.ApiAbandonedAnimalServiceImpl;
 
 @Controller
 public class HomeController {
@@ -15,4 +27,17 @@ public class HomeController {
 		return "index";
 	}
 	
+	@RequestMapping(value ="board", method = RequestMethod.GET)
+	public String board(Locale locale, Model model) {
+		
+		return "board";
+	}
+	
+	@RequestMapping("toAbandoned")
+	public String abandoned() {
+		return "abandoned";
+	}
+	
+	
+
 }
